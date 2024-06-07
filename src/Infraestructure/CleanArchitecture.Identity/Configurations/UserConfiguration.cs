72c1d2cc70e5ea3ba6a32ee468035092f3a531bc
Priscy Antonio Reales Mozo <realespriscy@hotmail.com>
2024-06-07 16:16:35 -0500

    Add IdentityDbContext and entity configurations

---
+++ src/Infraestructure/CleanArchitecture.Identity/Configurations/UserConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitecture.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Identity.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        var hasher = new PasswordHasher<ApplicationUser>();
+        builder.HasData(
+            new ApplicationUser
+            {
+                Id = "d10a2f15-a4ef-4f94-9e05-d116db6e5019",
+                Email = "admin@localhost.com",
+                NormalizedEmail = "admin@localhost.com",
+                Nombre = "Admin",
+                Apellidos = "Admin",
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                PasswordHash = hasher.HashPassword(null, "Adm1n*.")
+            },
+            new ApplicationUser
+            {
+                Id = "c8b996d7-e738-4b4c-aa17-8520360d394a",
+                Email = "soporte@localhost.com",
+                NormalizedEmail = "soporte@localhost.com",
+                Nombre = "Soporte",
+                Apellidos = "Soporte",
+                UserName = "soporte",
+                NormalizedUserName = "soporte",
+                PasswordHash = hasher.HashPassword(null, "Adm1n*.")
+            }
+        );
+    }
+}
---
+++ src/Infraestructure/CleanArchitecture.Identity/Configurations/UserRoleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchitecture.Identity.Configurations;
+
+public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+    {
+        builder.HasData(
+            new IdentityUserRole<string>
+            {
+                RoleId = "d81b8eea-04f1-436a-8662-73e8de1632bd",
+                UserId = "d10a2f15-a4ef-4f94-9e05-d116db6e5019"
+            },new IdentityUserRole<string>
+            {
+                RoleId = "775b4851-a07f-4326-b31d-ec20ec3be6c4",
+                UserId = "c8b996d7-e738-4b4c-aa17-8520360d394a"
+            }
+        );
+    }
+}
